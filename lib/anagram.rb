7919019da834c996accbaf4398dbe8f0a3a6b036
Timothy Miller <mr.tim.miller123@gmail.com>
2016-11-04 16:55:46 -0600

    Added some abstraction

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,11 +11,16 @@ class Anagram
   def match(list)
     @anagrams = []
     list.each do |x|
-      if x.split("").sort == @word.split("").sort
+      if x.split("").sort == word_sorter
         @anagrams << x
       end
     end
     @anagrams
+  end
+
+  protected
+  def word_sorter
+    @word.split("").sort
   end
 
 end
