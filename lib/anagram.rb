37e1cf67da09bb8d14ed85e40eb982b5b6c3827c
Timothy Miller <mr.tim.miller123@gmail.com>
2016-11-04 16:45:02 -0600

    Finished this Anagram matcher

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    @anagrams = []
+    list.each do |x|
+      if x.split("").sort == @word.split("").sort
+        @anagrams << x
+      end
+    end
+    @anagrams
+  end
+
+end
